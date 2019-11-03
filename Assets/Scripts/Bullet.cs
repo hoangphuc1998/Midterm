563833a39b22d59a5aa5507393e0431c70148c5c
Administrator <trangtrunghoangphuc@gmail.com>
2019-11-03 23:43:15 +0700

    Add player movement and shooting through network

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class Bullet : MonoBehaviourPun
+{
+    public float speed = 10f;
+
+    public float destroyTime = 2f;
+    IEnumerator destroyBullet()
+    {
+        yield return new WaitForSeconds(destroyTime);
+        if (GetComponent<PhotonView>().IsMine)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Start()
+    {
+        StartCoroutine("destroyBullet");
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Photon.Pun;
+using System;
+
+public class PlayerMovement : MonoBehaviourPun, IPunObservable
+{
+    public PhotonView photonview;
+    public float moveSpeed = 5f;
+    public float moveSmoothSpeed = 10f;
+    public float rotationSmoothSpeed = 10f;
+    private Rigidbody2D rb;
+    Vector2 movement;
+    private GameObject scenceCamera;
+    private Camera cam;
+    Vector2 mousePos;
+    Vector3 smoothMove;
+    Quaternion smoothRotation;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float bulletForce = 1f;
+
+    void Start()
+    {
+        if (photonView.IsMine)
+        {
+            rb = GetComponent<Rigidbody2D>();
+            scenceCamera = GameObject.Find("Main Camera");
+            cam = Camera.main;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            ProcessInput();
+        }
+        else
+        {
+            SmoothMovement();
+        }
+    }
+    private void ProcessInput()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            photonView.RPC("Shoot", RpcTarget.All, firePoint.position, firePoint.rotation, firePoint.up);
+        }
+    }
+
+    [PunRPC]
+    void Shoot(Vector3 pos, Quaternion rot, Vector3 upPos)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, pos, rot);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(upPos * bulletForce, ForceMode2D.Impulse);
+    }
+
+
+    private void SmoothMovement()
+    {
+        transform.position = Vector3.Lerp(transform.position, smoothMove, Time.deltaTime * moveSmoothSpeed);
+        transform.rotation = Quaternion.Lerp(transform.rotation, smoothRotation, Time.deltaTime * rotationSmoothSpeed);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else if (stream.IsReading)
+        {
+            smoothMove = (Vector3)stream.ReceiveNext();
+            smoothRotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
